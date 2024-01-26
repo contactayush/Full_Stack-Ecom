cda4ef018bfbde147587e3bfaa7e7df1d8bb154c
anu1818 <anuragwadikar568@gmail.com>
2024-01-26 17:31:21 +0530

    responsiveness updates

--- Frontend/src/Components/HomeInfo.jsx
+++ Frontend/src/Components/HomeInfo.jsx
@@ -10,23 +10,31 @@ import calvin from "../assets/calvin.png";
 const HomeInfo = () => {
   return (
     <>
-      <div className="con">
-        <img src={back_img} alt="back" className="back_img" />
-        <div className="con-content">
-          <div className="para">
-            <p className="p1">FIND CLOTHES THAT MATCHES YOUR STYLE</p>
+      <div className="con lg:flex flex-col flex">
+        <img
+          src={back_img}
+          alt="back"
+          className="back_img lg:w-[1519px] w-[390px] hidden lg:block"
+        />
+        <div className="con-content sm:w-[390px] bg-[#F2F0F1]">
+          <div className="lg:mt-[53px] lg:ml-[100px] mt-[53px] ml-[30px] w-[315px] h-[93px] lg:w-[620px] lg:h-[173px]">
+            <p className="p1 text-[36px] lg:text-[64px] leading-[34px] lg:leading-[64px] text-[#000000]">
+              FIND CLOTHES THAT MATCHES YOUR STYLE
+            </p>
           </div>
-          <div className="para2">
-            <p className="p2">
+          <div className="lg:mt-[45px] lg:ml-[100px] mt-[40px] ml-[30px] w-[358px] h-[70px] lg:w-[486px] lg:h-[60px] text-[#00000099]">
+            <p className="p2 leading-[20px] text-[14px] lg:text-[16px] lg:leading-[21px]">
               Browse through our diverse range of meticulously crafted garments,
               designed to bring out your individuality and cater to your sense
               of style.
             </p>
           </div>
           <div className="shop_now_btn">
-            <button className="shop_btn">SHOP NOW</button>
+            <button className="shop_btn lg:w-[210px] w-[358px] mt-[5px] lg:ml-[92px] ml-[26px]">
+              SHOP NOW
+            </button>
           </div>
-          <div className="content">
+          <div className="content flex-wrap lg:w-[596px] w-[472px] lg:h-[74px] h-[120px]">
             <div className="item">
               <p className="count">200+</p>
               <p className="item_text">International Brands</p>
@@ -36,16 +44,20 @@ const HomeInfo = () => {
               <p className="count">2000+</p>
               <p className="item_text">High-Quality Products</p>
             </div>
-            <div className="line"></div>
-            <div className="item">
+            <div className="line lg:block hidden"></div>
+            <div className="item lg:ml-0 ml-[96px]">
               <p className="count">30000+</p>
               <p className="item_text">Happy Customers</p>
             </div>
           </div>
         </div>
 
-        <img src={Vector} alt="vector" className="vector" />
-        <img src={small_star} alt="small_star" className="small_star" />
+        <img src={Vector} alt="vector" className="vector hidden lg:block" />
+        <img
+          src={small_star}
+          alt="small_star"
+          className="small_star hidden lg:block"
+        />
       </div>
       <div className="partners">
         <img src={Versace} alt="versace" className="patners_logo" />
--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -22,18 +22,25 @@ const MobileNavbar = () => {
 
   return (
     <>
-      <div className="hamburger lg:hidden" onClick={showDrawer}>
+      <div
+        className="hamburger lg:hidden hover:cursor-pointer mt-[10px]"
+        onClick={showDrawer}
+      >
         <Menu size={24} />
       </div>
       <Drawer
-        title="Mobile Menu"
+        title="Shop Menu"
         placement="left"
         closable={false}
         onClose={onClose}
         visible={open}
       >
         {elements.map((element, id) => {
-          return <p key={id}>{element}</p>;
+          return (
+            <p key={id} className="cursor-pointer">
+              {element}
+            </p>
+          );
         })}
       </Drawer>
     </>
@@ -45,7 +52,7 @@ const Navbar = () => {
     navigate("/cart");
   };
   return (
-    <header className="container  sm:w-[1240px] w-[390px] sm:ml-[100px] ml-[15px]">
+    <header className="container  lg:w-[1240px] w-[390px] lg:ml-[100px] ml-[15px] gap-[40px]">
       <div className="hamburger lg:hidden ">
         <MobileNavbar />
       </div>
@@ -55,13 +62,17 @@ const Navbar = () => {
       <div className="elements ">
         <ul className="element mt-[49px] lg:flex hidden">
           <li className="ele">
-            <div className="shop">
+            <div className="shop lg:flex hidden">
               <span>Shop</span>
               <img src={down_arrow} alt="down_arrow" className="down_arrow" />
             </div>
           </li>
           {elements.map((element, id) => {
-            return <li key={id}>{element}</li>;
+            return (
+              <li key={id} className="hover:cursor-pointer">
+                {element}
+              </li>
+            );
           })}
         </ul>
       </div>
@@ -76,22 +87,22 @@ const Navbar = () => {
       <input
         type="text"
         placeholder="Search for Products..."
-        className="input-box lg:flex hidden sm:flexhidden"
+        className="input-box lg:flex hidden "
       />
-      <div className="cart-user lg:w-[106px] w-[96px] lg:h-[32px] h-[24px]">
-        <div className="cart">
+      <div className="cart-user lg:w-[106px] w-[96px] lg:h-[32px] h-[24px] gap-[20px] mt-[10px]">
+        <div className="w-[24px] h-[24px]">
           <img
             src={cart}
             alt="cart"
-            className="cart-logo hover:cursor-pointer"
+            className="w-[20.27px] h-[20.27px] hover:cursor-pointer"
             onClick={navigateHandler}
           />
         </div>
-        <div className="user">
-          <img src={user} alt="user" className="user-logo" />
+        <div className="w-[24px] h-[24px]">
+          <img src={user} alt="user" className="w-[20.27px] h-[20.27px]" />
         </div>
-        <div className="search lg:hidden block">
-          <img src={search} alt="search-icon" />
+        <div className="search lg:hidden ">
+          <img src={search} alt="search-icon " />
         </div>
       </div>
     </header>
